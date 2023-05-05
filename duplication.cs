d129a0a0500f7e904bb33c870629b68ff1b7015e
Invia1 <132645377+Invia1@users.noreply.github.com>
2023-05-05 16:12:50 +0530

    Add files via upload

---
+++ duplication.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class lokesh
+    {
+      static void Main()
+        {
+            Console.WriteLine("Enter you input");
+            string s = Console.ReadLine();
+            int cnt = 1;
+            int cou = 1;
+            for(int i=0; i<s.Length; i++)
+            {
+                for(int j=i+1; j<s.Length; j++)
+                {
+                    if (s[i] == s[j])
+                    {
+                        cnt++;
+                        Console.WriteLine(s[j]);
+                    }
+                    else
+                    {
+                        Console.WriteLine(s[j]+" "+cou);
+                    }
+                }
+
+            }
+
+            Console.Write("The char comes in string:" + cnt);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ static var.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+   class kailash
+    {
+        static readonly int a=10;
+        static  kailash()
+        {
+            kailash.a = 20;
+        }
+        static void Main()
+        {
+            int res  =kailash.a;
+            Console.WriteLine(res);
+            Console.ReadLine();
+        }
+    }
+}
