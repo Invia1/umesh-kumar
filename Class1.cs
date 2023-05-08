b7a148c6b704b11f3aa1497e705ca2bff8095e6c
Invia1 <132645377+Invia1@users.noreply.github.com>
2023-05-08 09:10:42 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    
+        class office
+        {
+            static readonly int ID;
+            static office()
+            {
+                office.ID = 45;
+            }
+            static void print()
+            {
+                Console.WriteLine("The Id of the person is:" + office.ID);
+            }
+            public static void Main()
+            {
+                office.print();
+                Console.ReadLine();
+            }
+        }
+    }
+
+
