e0a13d8a100c33ef90c5c9792450a0b4a62ff816
Invia1 <132645377+Invia1@users.noreply.github.com>
2023-05-19 10:43:41 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day11
+{
+    class Program
+    {
+     static void Main(string[] args)
+        {
+            List<string> objlist = new List<string>();
+            objlist.Add("umesh");
+            objlist.Add("lokesh");
+            objlist.Add("pawan");
+            objlist.Add("sansank");
+            objlist.Add("pawan");
+            objlist.Add("kailash");
+
+            //Query to fetch the data
+            var temp = from obj in objlist where obj=="pawan" select obj;
+                     
+            foreach(var t in temp)
+            {
+                Console.WriteLine(t + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
